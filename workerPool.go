9ec40f89effb2375af26b53cb4ce0338d581c910
Quazi Minhajuddin <minhajqazi@live.com>
2023-08-06 17:14:52 +0530

    added support for in order job execution

--- workerPool.go
+++ workerPool.go
@@ -8,71 +8,79 @@ import (
 	"sync"
 )
 
-type JobRequest[T any] struct {
+type Data interface {
+	any
+}
+
+type JobRequest[T Data] struct {
 	Ctx     context.Context
 	Request T
 }
 
-type InOrderJobRequest[T any] struct {
+type InOrderJobRequest[T Data] struct {
 	JobRequest JobRequest[T]
 	Index      int32
 }
 
-func NewJobRequest[T any](request T) *JobRequest[T] {
+func NewJobRequest[T Data](request T) *JobRequest[T] {
 	return &JobRequest[T]{Request: request}
 }
 
-func NewJobRequestWithContext[T any](ctx context.Context, request T) *JobRequest[T] {
+func NewJobRequestWithContext[T Data](ctx context.Context, request T) *JobRequest[T] {
 	return &JobRequest[T]{Ctx: ctx, Request: request}
 }
 
-type JobResponse[R any] struct {
+type JobResponse[R Data] struct {
 	Response R
 	Error    error
 }
 
-type JobFunc[JobRequest, JobResponse any] func(JobRequest) JobResponse
+type JobFunc[T Data, R Data] func(JobRequest[T]) JobResponse[R]
 
-type WorkerPool[JobRequest, JobResponse any] interface {
+type WorkerPool[T Data, R Data] interface {
 	NumberOfWorkers() int32
 	AddWorkers(int32)
 	RemoveWorkers(int32)
 	SetNumberOfWorkers(int32)
 	WorkerScheduler(context.Context)
-	SetJobFunc(JobFunc[JobRequest, JobResponse]) WorkerPool[JobRequest, JobResponse]
-	AddJobs(jobRequests ...JobRequest) WorkerPool[JobRequest, JobResponse]
+	SetJobFunc(JobFunc[T, R]) WorkerPool[T, R]
+	AddJobs(...JobRequest[T]) WorkerPool[T, R]
 	RunInBackground()
+	RunInOrder() WorkerPool[T, R]
+	ForEach(f func(JobResponse[R]))
 }
 
-type workerPool[JobRequest, JobResponse any] struct {
+type workerPool[T Data, R Data] struct {
 	wg                 *sync.WaitGroup
 	numberOfWorkers    int32
-	jobRequests        []JobRequest
-	jobFunc            JobFunc[JobRequest, JobResponse]
-	jobQueue           chan JobRequest
-	backgroundJobQueue chan JobRequest
-	inOrderJobQueue    chan InOrderJobRequest[JobRequest]
-	jobResponseChan    chan JobResponse
+	jobRequests        []JobRequest[T]
+	jobFunc            JobFunc[T, R]
+	jobQueue           chan JobRequest[T]
+	backgroundJobQueue chan JobRequest[T]
+	inOrderJobQueue    chan InOrderJobRequest[T]
+	jobResponseChan    chan JobResponse[R]
 	addWorkerChan      chan struct{}
 	removeWorkerChan   chan struct{}
 	cond               *sync.Cond
-	counter            int
+	counter            int32
 }
 
-func New[JobRequest, JobResponse any](workerCtx context.Context, numberOfWorkers int32) WorkerPool[JobRequest, JobResponse] {
+func New[T Data, R Data](workerCtx context.Context,
+	numberOfWorkers int32) WorkerPool[T, R] {
 	if numberOfWorkers <= 0 {
 		numberOfWorkers = 1
 	}
-	wp := &workerPool[JobRequest, JobResponse]{
+	wp := &workerPool[T, R]{
 		numberOfWorkers:    0,
-		jobQueue:           make(chan JobRequest, 3*numberOfWorkers),
-		backgroundJobQueue: make(chan JobRequest, 3*numberOfWorkers),
-		jobResponseChan:    make(chan JobResponse, numberOfWorkers),
-		addWorkerChan:      make(chan struct{}),
-		removeWorkerChan:   make(chan struct{}),
-		wg:                 new(sync.WaitGroup),
-		cond:               sync.NewCond(new(sync.Mutex)),
-		counter:            0,
+		jobQueue:           make(chan JobRequest[T], 3*numberOfWorkers),
+		backgroundJobQueue: make(chan JobRequest[T], 3*numberOfWorkers),
+		inOrderJobQueue:    make(chan InOrderJobRequest[T], 3*numberOfWorkers),
+		// jobResponseChan:    make(chan JobResponse[R], numberOfWorkers),
+		addWorkerChan:    make(chan struct{}),
+		removeWorkerChan: make(chan struct{}),
+		wg:               new(sync.WaitGroup),
+		cond:             sync.NewCond(new(sync.Mutex)),
+		counter:          0,
 	}
 	// launch workers
 	go wp.WorkerScheduler(workerCtx)
@@ -82,11 +90,11 @@ func New[JobRequest, JobResponse any](workerCtx context.Context, numberOfWorkers
 	return wp
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) NumberOfWorkers() int32 {
+func (wp *workerPool[T, R]) NumberOfWorkers() int32 {
 	return wp.numberOfWorkers
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) WorkerScheduler(workerCtx context.Context) {
+func (wp *workerPool[T, R]) WorkerScheduler(workerCtx context.Context) {
 	for {
 		select {
 		case <-wp.addWorkerChan:
@@ -95,7 +103,7 @@ func (wp *workerPool[JobRequest, JobResponse]) WorkerScheduler(workerCtx context
 	}
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) SetNumberOfWorkers(count int32) {
+func (wp *workerPool[T, R]) SetNumberOfWorkers(count int32) {
 	if wp.numberOfWorkers == count {
 		return
 	}
@@ -107,17 +115,18 @@ func (wp *workerPool[JobRequest, JobResponse]) SetNumberOfWorkers(count int32) {
 	wp.AddWorkers(count - wp.numberOfWorkers)
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) AddWorkers(count int32) {
+func (wp *workerPool[T, R]) AddWorkers(count int32) {
 	if count <= 0 {
 		return
 	}
 	for i := int32(0); i < count; i++ {
 		wp.addWorkerChan <- struct{}{}
 	}
+
 	wp.numberOfWorkers += count
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) RemoveWorkers(count int32) {
+func (wp *workerPool[T, R]) RemoveWorkers(count int32) {
 	if wp.jobFunc == nil {
 		panic(errors.New("JobFunc is nil"))
 	}
@@ -133,19 +142,21 @@ func (wp *workerPool[JobRequest, JobResponse]) RemoveWorkers(count int32) {
 	wp.numberOfWorkers -= count
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) SetJobFunc(jobFunc JobFunc[JobRequest, JobResponse]) WorkerPool[JobRequest, JobResponse] {
+func (wp *workerPool[T, R]) SetJobFunc(
+	jobFunc JobFunc[T, R]) WorkerPool[T, R] {
 	wp.jobFunc = jobFunc
 
 	return wp
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) AddJobs(jobRequests ...JobRequest) WorkerPool[JobRequest, JobResponse] {
+func (wp *workerPool[T, R]) AddJobs(
+	jobRequests ...JobRequest[T]) WorkerPool[T, R] {
 	wp.jobRequests = append(wp.jobRequests, jobRequests...)
 
 	return wp
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) RunInBackground() {
+func (wp *workerPool[T, R]) RunInBackground() {
 	go func() {
 		for _, jobRequest := range wp.jobRequests {
 			fmt.Println("scheduling job:", jobRequest)
@@ -157,23 +168,23 @@ func (wp *workerPool[JobRequest, JobResponse]) RunInBackground() {
 	}()
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) RunInOrder() WorkerPool[JobRequest, JobResponse] {
-	wp.jobResponseChan = make(chan JobResponse, len(wp.jobQueue))
+func (wp *workerPool[T, R]) RunInOrder() WorkerPool[T, R] {
+	wp.jobResponseChan = make(chan JobResponse[R], len(wp.jobQueue))
 	go func() {
-		for _, jobRequest := range wp.jobRequests {
+		for index, jobRequest := range wp.jobRequests {
 			fmt.Println("scheduling job:", jobRequest)
 			wp.wg.Add(1)
-			wp.jobQueue <- jobRequest
+			wp.inOrderJobQueue <- InOrderJobRequest[T]{JobRequest: jobRequest, Index: int32(index)}
 		}
 		wp.wg.Wait()
-		fmt.Println("Jobs Done!!!")
+		fmt.Println("InOrder Jobs Done!!!")
 		close(wp.jobResponseChan)
 	}()
 
 	return wp
 }
 
-func (wp *workerPool[JobRequest, JobResponse]) ForEach(f func(JobResponse)) {
+func (wp *workerPool[T, R]) ForEach(f func(JobResponse[R])) {
 	for v := range wp.jobResponseChan {
 		f(v)
 	}
@@ -206,6 +217,26 @@ func (wp *workerPool[T, R]) worker(workerCtx context.Context) {
 				jobResponse := wp.jobFunc(jobRequest)
 				wp.jobResponseChan <- jobResponse
 				fmt.Println("req:", jobRequest, " res:", jobResponse)
+			}()
+		case inOrderJobRequest := <-wp.inOrderJobQueue:
+			func() {
+				defer wp.wg.Done()
+				defer func() {
+					if err := recover(); err != nil {
+						log.Println(err)
+					}
+				}()
+				jobResponse := wp.jobFunc(inOrderJobRequest.JobRequest)
+				wp.cond.L.Lock()
+				for wp.counter != inOrderJobRequest.Index {
+					wp.cond.Wait()
+				}
+				wp.cond.L.Unlock()
+				wp.jobResponseChan <- jobResponse
+				wp.cond.L.Lock()
+				wp.counter++
+				wp.cond.Broadcast()
+				wp.cond.L.Unlock()
 			}()
 		case <-workerCtx.Done():
 			fmt.Println("Worker Context cancelled")
